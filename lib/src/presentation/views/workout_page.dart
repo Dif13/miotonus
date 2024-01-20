f80c4c272689bf4dd50d165c79b70c0baf56b95a
Ruzal Gimazov <ruzal.gimazov@ya.ru>
2024-01-20 21:36:55 +0300

    Stable. Добавлена таблица с одной единственной обновляеой строкой состояния

--- lib/src/presentation/views/workout_page.dart
+++ lib/src/presentation/views/workout_page.dart
@@ -5,6 +5,7 @@ import 'package:miotonus/src/presentation/cubits/height_cubit.dart';
 import 'package:miotonus/src/presentation/widgets/workout_form_field_height.dart';
 import 'package:miotonus/src/utils/constants/nums.dart';
 import 'package:miotonus/src/utils/constants/strings.dart';
+import 'package:intl/intl.dart';
 
 class WorkoutPage extends StatefulWidget {
   const WorkoutPage({super.key, required this.title});
@@ -27,20 +28,65 @@ class _WorkoutPageState extends State<WorkoutPage> {
         title: Text(widget.title),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
         children: [
-          formFields(),
-          BlocBuilder<HeightCubit, Height>(
-              bloc: heightCubit,
-              builder: (context, height) {
-                return Column(
-                  children: [
-                    Text(height.localMin.toString()),
-                    Text(height.localMax.toString()),
-                  ],
-                );
-              }),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              formFields(),
+              BlocBuilder<HeightCubit, Height>(
+                  bloc: heightCubit,
+                  builder: (context, height) {
+                    return Column(
+                      children: [
+                        Text(height.localMin.toString()),
+                        Text(height.localMax.toString()),
+                        Table(
+                          border: TableBorder.all(),
+                          // columnWidths: const <int, TableColumnWidth>{
+                          //   0: IntrinsicColumnWidth(),
+                          //   1: FlexColumnWidth(),
+                          //   2: FixedColumnWidth(64),
+                          // },
+                          defaultVerticalAlignment:
+                              TableCellVerticalAlignment.middle,
+                          children: <TableRow>[
+                            TableRow(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SizedBox(
+                                    child: Center(
+                                      child: Text(DateFormat.Hm()
+                                          .format(DateTime.now())),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SizedBox(
+                                    child: Center(
+                                        child:
+                                            Text(height.localMin.toString())),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SizedBox(
+                                    child: Center(
+                                        child:
+                                            Text(height.localMax.toString())),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  }),
+            ],
+          ),
         ],
       ),
     );
